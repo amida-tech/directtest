99a00c053e8a0d0a4ddc0aad58f9b7a3598d8349
au2 <afsin@amida-tech.com>
2014-03-03 21:55:14 -0500

    Handling the case when direct messages are between the same domain is added.

--- app.js
+++ app.js
@@ -33,10 +33,20 @@ var email = {
 		attachment: "/Work/nodespace/directtest/resource/sample.txt"
 };
 
+var email2 = {
+		actual: {
+			from : "provider@expl-provider.amida-demo.com",
+			to : "provider2@expl-provider.amida-demo.com",
+			subject: "Test Message " + Date.now(),
+			body: "Here goes the message body"
+		},
+		attachment: "/Work/nodespace/directtest/resource/sample.txt"
+};
 
-mh.execute(sendingServer, receivingServer, email);
+//mh.execute(sendingServer, receivingServer, email);
+mh.execute(sendingServer, sendingServer, email2);
 
-//mh.deleteAllEmails(sendingServer);
+//mh.showAllEmails(receivingServer);
 
 mh.emitter.on('error', function(result) {
 	console.error(">>>>> Error");
