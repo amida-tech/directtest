226afe1024748f4bdcfb0fd0efc343a4199026fe
au2 <afsin@amida-tech.com>
2014-03-05 17:02:39 -0500

    initial version of binary file put and get is implemented.

--- keyserver.js
+++ keyserver.js
@@ -2,33 +2,38 @@
  * Key server generates and provides key files to the 
  */
 
-var http = require('http');
-
-var server = http.createServer();
-
-var request = require('request');
+var restify = require('restify');
 var fs = require('fs');
 
-server.on('request', function(req, res) {
-	switch (req.method) {
-	case 'POST':
-		console.log("In PUT");
-		var item = '';
-		req.setEncoding('utf8');
-		req.on('data', function(chunk) {
-			item += chunk;
-		});
-		req.on('end', function() {
-			console.log(item);
-			res.end('OK\n');
-		});
-		break;
-	case 'GET':
-		console.log("In GET");
-		res.writeHead(200, {'Content-Type': 'text/plain'});
-		res.end('Hello World\n');
-		break;
-	}
-});
+var server = restify.createServer();
+
+var errorHandler = function errorHandler(err) {
+	console.log(err);
+};
+
+function putFile(req, res, next) {
+	console.log('in putfile');
+
+	var data = new Buffer('');
+	
+	req.on('data', function(chunk) {
+		data = Buffer.concat([data, chunk]);
+	});
+	
+	req.on('end', function() {
+		fs.writeFile('/Work/sandbox/serverin/patient.der', data, 'binary', errorHandler);
+		res.send(200);
+	});
+}
+
+function getFile(req, res, next) {
+	fs.readFile('/Work/sandbox/serverout/provider.der', function(err, data) {
+		res.setHeader('content-type', 'application/octet-stream');
+		res.send(data);
+	});
+}
+
+server.put('/certificate', putFile);
+server.get('/certificate', getFile);
 
 server.listen(3000);
