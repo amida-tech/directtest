fadcad3aaf4f27b763d7790e8a90973f78046328
au2 <afsin@amida-tech.com>
2014-03-12 22:07:43 -0400

    - client parts of trust bundle/anchor add/remove - refactor certification add (seperate file put/get)

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var mh = require("./lib/mailbbtestcase");
 var certificatemgr = require('./lib/certificatemgr');
+var request = require('request');
 
 var sendingServer = {
 		ip : "expl-provider.amida-demo.com",
@@ -63,11 +64,33 @@ mh.emitter.on('end', function(result) {
 //getCertificate('localhost', 3000);
 //certificatemgr.putCertificate('/Work/sandbox/clientout/patient.der', 'localhost', 3000);
 //certificatemgr.reset('expl-provider.amida-demo.com', 3000);
+//certificatemgr.getCertificate('/Work/sandbox/serverin/patient.der', 'localhost', 3000);
 
 var info = {
 	domain: "expl-provider.amida-demo.com",
 	country: "US",
 	city: "Rockville",
-	organization: "Amida Provider Services"
+	organization: "Amida Provider Services",
+	x509: "dcertificate",
+	pkcs12: "dprivcert",
+	request: "dreq.pem",
+	trustUrl: "https://secure.bluebuttontrust.org/p7b.ashx?id=cb300117-3a4a-e211-8bc3-78e3b5114607",
+	trustName: "test",
+	owner: "expl-provider.amida-demo.com",
+	filename: "patient.der"
 };
-certificatemgr.createPKCS10('localhost', 3000, info);
+//certificatemgr.createPKCS10('localhost', 3000, info);
+//certificatemgr.createX509('localhost', 3000, info);
+//certificatemgr.createPKCS12('localhost', 3000, info);
+//certificatemgr.postTrustBundle('expl-provider.amida-demo.com', 3000, info);
+//certificatemgr.postAnchor('expl-provider.amida-demo.com', 3000, info);
+//certificatemgr.postCertificate('expl-provider.amida-demo.com', 3000, info);
+request.del("http://expl-provider.amida-demo.com:3000/reset");
+
+var address = "http://54.201.106.201:3000/bundle";
+//var address = "https://secure.bluebuttontrust.org/p7b.ashx?id=cb300117-3a4a-e211-8bc3-78e3b5114607";
+//request.get(address, function(error, response, body) {
+//	console.log(error);
+//	console.log(response);
+//});
+
